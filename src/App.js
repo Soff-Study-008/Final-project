803915de65b44aa35a93c6c4dc8f76e166b1f7bb
sunnat1989s <saydaliyevs188@gmail.com>
2022-09-30 16:42:26 +0500

    sunnat aka qoshgan tag

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Sunnat aka qoshgan tag</h1>
       <button>mustafo qoshdi</button>
+      <p>dfghjdfghjk </p>
       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, temporibus iusto illum voluptatem voluptatum magni cumque laudantium? Quibusdam exercitationem quasi officiis nemo sequi doloremque, recusandae maiores magnam quo, blanditiis eligendi!</h1>
     </div>
   );
