82bb8ec350322d0acd12e049fe3cf8fc0052fd54
Erkin303m <maxmudjonoverkin189@gmail.com>
2022-09-30 16:18:57 +0500

    sdcs

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, temporibus iusto illum voluptatem voluptatum magni cumque laudantium? Quibusdam exercitationem quasi officiis nemo sequi doloremque, recusandae maiores magnam quo, blanditiis eligendi!</h1>
     </div>
   );
 }
