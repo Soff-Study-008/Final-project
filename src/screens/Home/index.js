4d787f3e3a939ffb6a5ff5252a79800f68923c51
Erkin303m <maxmudjonoverkin189@gmail.com>
2022-10-03 14:20:29 +0500

    sdvsdv

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default Home
