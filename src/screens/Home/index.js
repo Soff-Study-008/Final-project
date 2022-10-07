91aebb0c773f7d454371c9c1cf0928df3a8e9cf8
Erkin303m <maxmudjonoverkin189@gmail.com>
2022-10-07 17:54:35 +0500

    sdsdv

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import Slider from "react-slick";
 import { FaSearch } from 'react-icons/fa';
 import All from '../../components/All'
 import "./home.css"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useNavigate } from 'react-router-dom';
+import Slider from 'react-slick';
 
 // import Slider from "react-slick";
 // import { AiFillPlayCircle } from "react-icons/ai";
@@ -59,6 +59,41 @@ const Home = () => {
         ]
     }
 
+
+    let settings2 = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 7,
+        slidesToScroll: 7,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    }
 
 
 
@@ -190,8 +225,23 @@ const Home = () => {
                         </div>
                     </div>
 
-                    <h1>Pop music</h1>
+                    <div className="section_top">
+                        <div className="soz_for">
+                            {/* <h1 className='gl_title'>MUsic</h1> */}
+                            <img src="https://media.graphassets.com/4L03glPTAe3c3Ongtpzh" alt="" className="top_img" />
+                            <p className='music_avtor'>Ved and Tara fall in love while on a holiday in Corsica and decide to keep their real identities undisclosed. Tara returns to Delhi and meets a new Ved, who is trying to discover his true self. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate velit voluptates tempora reiciendis animi quae repellendus, vero maiores fugit officiis quis nisi assumenda dolor incidunt est optio, porro debitis ducimus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum aliquid, veritatis culpa dolores eos tempora modi accusamus necessitatibus veniam voluptate quas sequi cum aperiam recusandae quaerat nihil ex dolor cumque consequuntur corrupti minus placeat earum? Nam quas earum doloribus tempore!</p>
+                            <p className='text_red'>GENRES</p>
+                            <p className='text_wight'>Senior Veteran</p>
+                            <button className='bt_watch'>
+                                WATCH
+                                <img src="bt_fr.png" alt="" className='bt_img' />
+                            </button>
+                            <button className='bt_mylist'>MY LIST <span>+</span> </button>
+                        </div>
+                    </div>
+                    <div className="row">
                     <Slider {...settings}>
+
                         {
                             (miksFilter.length > 0) ? (
                                 miksFilter.map((v, i) => {
@@ -255,7 +305,7 @@ const Home = () => {
                     <div>
                         <div className="slider_map">
                             <div>
-                                <Slider {...settings}>
+                                <Slider {...settings2}>
                                     {
                                         (data.length > 0) ? (
                                             data.map((v, i) => {
@@ -277,6 +327,7 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
+                </div>
                 </div>
 
             </section>
