8cfe2f0229e93031f6cfd5964368bf3bdfac52bc
Erkin303m <maxmudjonoverkin189@gmail.com>
2022-10-07 15:28:21 +0500

    sdsdv

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -55,19 +55,12 @@ const Home = () => {
 
     }
 
-<<<<<<< HEAD
     const navigate = useNavigate();
 
     const Going = async (name) => {
         console.log(name);
 
         let artistParametres = {
-=======
-    const navigate = useNavigate()
-    const Going = async (ID) => {
-        console.log(ID);
-        var artistParametres = {
->>>>>>> 82b544e329af2db207bb4d3db150a303860ef55f
             method: "GET",
             headers: {
                 "Content-type": "application/json",
@@ -82,7 +75,6 @@ const Home = () => {
                     state: data.tracks.items
                 })
             })
-<<<<<<< HEAD
 
     }
 
@@ -128,78 +120,11 @@ const Home = () => {
             }
         ]
     };
-=======
-    }
-
-
-    // var settings = {
-    //     dots: false,
-    //     infinite: false,
-    //     speed: 500,
-    //     slidesToShow: 6,
-    //     slidesToScroll: 3,
-    //     initialSlide: 0,
-    //     responsive: [
-    //         {
-    //             breakpoint: 1024,
-    //             settings: {
-    //                 slidesToShow: 3,
-    //                 slidesToScroll: 3,
-    //                 infinite: true,
-    //                 dots: true
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 600,
-    //             settings: {
-    //                 slidesToShow: 2,
-    //                 slidesToScroll: 2,
-    //                 initialSlide: 2
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 480,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 slidesToScroll: 1
-    //             }
-    //         }
-    //     ]
-    // };
->>>>>>> 82b544e329af2db207bb4d3db150a303860ef55f
 
     return (
         <All>
             <section className='fon_home'>
                 <div className="container">
-<<<<<<< HEAD
-                    <input type="input" placeholder='search' onChange={(v) => setInputWord(v.target.value)} onKeyPress={event => {
-                        if (event.key == "Enter") {
-                            Searching()
-                        }
-                    }} />
-                    <button onClick={Searching}>Search</button>
-                    <div className="row">
-                        {
-                            (data.length > 0) ? (
-                                data.map((v, i) => {
-                                    return <div key={i} className="col-3">
-                                        <div className="card">
-                                            <img src={v.images[0].url} alt="" />
-                                            <h1>{v.name}</h1>
-                                            <button onClick={() => Going(v.name)} className="btn btn-warning">Play</button>
-                                        </div>
-
-                                    </div>
-                                })
-                            ) : (
-                                <h1>error</h1>
-                            )
-                        }
-                    </div>
-
-=======
->>>>>>> 82b544e329af2db207bb4d3db150a303860ef55f
                     <div className="row">
                         <div className="col-12 soz_for">
                             <div className="row">
